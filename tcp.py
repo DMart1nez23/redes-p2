d8faa622c2e891c5e4f5e040a08a2b7ae957d5a8
DMart1nez23 <150975758+DMart1nez23@users.noreply.github.com>
2025-07-16 15:24:46 -0300

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -35,8 +35,12 @@ class Servidor:
             # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
             # TODO: talvez você precise passar mais coisas para o construtor de conexão
             conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao)
-            # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
-            # fazer aqui mesmo ou dentro da classe Conexao.
+            
+            ack_no = seq_no + 1
+            syn_ack_header = make_header(dst_port, src_port, seq_no, ack_no, FLAGS_SYN | FLAGS_ACK)
+            syn_ack_segment = fix_checksum(syn_ack_header, src_addr, dst_addr)
+            self.rede.enviar(syn_ack_segment, src_addr)
+            
             if self.callback:
                 self.callback(conexao)
         elif id_conexao in self.conexoes:
@@ -78,14 +82,37 @@ class Conexao:
         """
         Usado pela camada de aplicação para enviar dados
         """
-        # TODO: implemente aqui o envio de dados.
-        # Chame self.servidor.rede.enviar(segmento, dest_addr) para enviar o segmento
-        # que você construir para a camada de rede.
-        pass
+        src_addr, src_port, dst_addr, dst_port = self.id_conexao
+
+        offset = 0
+        while offset < len(dados):
+            payload = dados[offset:offset + MSS]
+            header = make_header(dst_port, src_port, self.prox_seq, self.prox_ack, FLAGS_ACK)
+            segment = fix_checksum(header + payload, dst_addr, src_addr)
+
+            self.servidor.rede.enviar(segment, src_addr)
+            print(f'Enviando pacote: Seq={self.prox_seq}, Tam={len(payload)}')
+
+            # Atualiza o número de sequência e os dados não reconhecidos
+            self.prox_seq += len(payload)
+            self.dados_nao_confirmados += payload
+            offset += MSS
+
+            # Inicia o temporizador de timeout se não estiver rodando
+            if not self.timer:
+                self.timer = asyncio.get_event_loop().call_later(0.5, self.handle_timeout)
 
     def fechar(self):
         """
         Usado pela camada de aplicação para fechar a conexão
         """
-        # TODO: implemente aqui o fechamento de conexão
-        pass
+        src_addr, src_port, dst_addr, dst_port = self.id_conexao
+
+        fin_header = make_header(dst_port, src_port, self.prox_seq, self.prox_ack, FLAGS_FIN)
+        fin_segment = fix_checksum(fin_header, dst_addr, src_addr)
+
+        self.servidor.rede.enviar(fin_segment, src_addr)
+        print(f'Enviando FIN: Seq={self.prox_seq}, Ack={self.prox_ack}')
+
+        if not self.timer:
+            self.timer = asyncio.get_event_loop().call_later(1, self.handle_timeout)
